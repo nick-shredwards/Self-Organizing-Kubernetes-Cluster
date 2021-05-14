d564d4930ab181a0f39359b074256b5143b4ca79
Marshall Quist <quist1@stolaf.edu>
2021-05-14 12:31:55 -0500

    making sure script is run as sudo

--- add_workers.sh
+++ add_workers.sh
@@ -1,12 +1,21 @@
 #!/bin/bash
-awk 'NR!=1' hostfile > ~/nodes.txt
-echo "input username"
-read $user
-while read ip
-do
-    echo "line: $ip"
-    kubeadm token create --print-join-command > "joincommand-$ip.sh"
-    chmod u+x "joincommand-$ip.sh"
-    scp "joincommand-$ip.sh" "$user@$ip"
-    ssh "$user@$ip" sudo ./"joincommand-$ip.sh"
-    done < ~/nodes.txt
+add_workers(){
+    awk 'NR!=1' hostfile > ~/nodes.txt
+    echo "input username"
+    read $user
+    while read ip
+    do
+	echo "line: $ip"
+	kubeadm token create --print-join-command > "joincommand-$ip.sh"
+	chmod u+x "joincommand-$ip.sh"
+	scp "joincommand-$ip.sh" "$user@$ip"
+	ssh "$user@$ip" sudo ./"joincommand-$ip.sh"
+        done < ~/nodes.txt
+}
+
+if [ "$EUID" -ne 0 ]
+then
+    echo "Please run as root"
+else
+    add_workers
+fi
