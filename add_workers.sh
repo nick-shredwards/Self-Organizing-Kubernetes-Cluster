57c4eeb62bfae7ded37a8671804ff83bf18b80de
Marshall Quist <quist1@stolaf.edu>
2021-05-21 13:14:51 -0500

    fixing password request field

--- add_workers.sh
+++ add_workers.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 add_workers(){
-    awk 'NR!=1' hostfile > ~/nodes.txt
+    awk 'NR!=1' hostfile_temp > ~/nodes.txt
     echo "input username"
     read user
+    echo "input password"
+    read -s password
     while read ip
     do
 	echo "ip: $ip" 
 	x=$(kubeadm token create --print-join-command)
-	echo "input password:"
-	read password
 	ssh "$user@$ip" "echo $password | sudo -S $x"
         done < ~/nodes.txt
 }
